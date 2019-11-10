6a23190de030f2b940aa4bb22c03ce5111f6d295
Sonic12040 <Daniel.g.Richmond@gmail.com>
2019-11-10 11:34:53 -0600

    feat: added styled button

--- packages/styled-components-playground/src/App.js
+++ packages/styled-components-playground/src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+
+const Button = styled.button`
+  display: inline-block;
+  background-color: #004d00;
+  color: #ffffff;
+  padding: 16px;
+  border: 2px solid #ffffff;
+  font-family: sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  margin: 16px;
+  text-decoration: none;
+  text-transform: uppercase;
+  &:hover {
+    background-color: #00695c;
+    border: 2px solid #000000;
+  }
+  &:focus {
+    background-color: #00695c;
+    border: 2px solid #000000;
+  }
+  &:active {
+    background-color: #004d00;
+    border: 2px solid #000000;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>Click Me!</Button>
     </div>
   );
 }
